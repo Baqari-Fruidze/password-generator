15d0efbd9e73f0452806509c00ddc739e4baf59d
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-31 18:12:39 +0400

    button half fixed,copy made

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("ho");
-// clipboard copy js
+// let copyButton = document.querySelector(".svg");
+// copyButton.addEventListener("click", () => {
+//   let text = document.querySelector(".top-password");
+//   text.select();
+//   Text.setSelectionRange(0, 99999);
+//   navigator.clipboard.writeText(Text.value);
+//   alert("Copied the text: " + Text.value);
+// });
+
+let copyButton = document.querySelector(".svg");
+copyButton.addEventListener("click", () => {
+  let text = document.querySelector(".top-password");
+  text.select();
+  document.execCommand("copy");
+  alert("Copied the text: " + text.value);
+});
